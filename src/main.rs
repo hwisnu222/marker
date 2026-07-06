045a3330bca49fb1a2397a3f473fa9089c1839f5
Wisnu Box <hwisnu222@gmail.com>
2026-07-06 14:39:20 +0700

    initial code

---
+++ src/main.rs
@@ -0,0 +1,123 @@
+use std::{ error::Error, net::SocketAddr, sync::{Arc, Mutex}};
+
+use jsonrpsee::{proc_macros::rpc, server::{Server}, types::ErrorObjectOwned};
+use rusqlite::{Connection};
+use serde::Serialize;
+use tower_http::cors::{Any, CorsLayer};
+use hyper::Method;
+
+use crate::repository::{Bookmark, BookmarkRepository};
+
+pub mod repository;
+
+#[derive(Clone, Serialize)]
+pub struct Response{
+    message: String,
+}
+
+#[derive(Clone, Serialize)]
+pub struct UrlResponse{
+    url: String
+}
+
+#[rpc(server)]
+trait RpcApi {
+    #[method(name="add_url")]
+    fn add_url(&self,title: String, url: String) -> Result<Response, ErrorObjectOwned>;
+    
+    #[method(name="get_urls")]
+    fn get_urls(&self) -> Result<Vec<Bookmark>, ErrorObjectOwned>;
+
+    #[method(name="delete_url")]
+    fn delete_url(&self, id: String) -> Result<Response, ErrorObjectOwned>;
+
+
+}
+
+struct RpcImplent{
+    bookmark_repo: BookmarkRepository
+}
+
+// because jsonrpsee change name impl from struct
+// name struct in impl adding suffix `Server`
+// so when your trait have name `RpcApi`
+// then jsonrpsee will change the name of `impl`` is `RpcApiServer``
+impl RpcApiServer for RpcImplent{
+    fn add_url(&self, title: String, url: String) -> Result<Response, jsonrpsee::types::ErrorObjectOwned>{
+
+        match self.bookmark_repo.add(title, url){
+            Ok(status) => {
+                let res = Response{
+                    message: status
+                };
+                Ok(res)
+            }
+            Err(e)=> {
+                eprintln!("Error: {}", e);
+                let err = ErrorObjectOwned::owned(-1, "failed add url", None::<()>);
+                Err(err)
+            }
+        }
+    }
+
+    fn get_urls(&self) -> Result<Vec<Bookmark> ,ErrorObjectOwned> {
+        match self.bookmark_repo.get() {
+            Ok(data) =>{
+                Ok(data)
+            }
+            Err(e )=>{
+                eprintln!("Error: {}", e);
+                Err(ErrorObjectOwned::owned(-1, "failed get bookmarks", None::<()>))
+            }
+        }
+    }
+
+    fn delete_url(&self,id:String) -> Result<Response,ErrorObjectOwned> {
+        Ok(Response{
+            message: format!("success delete id: {}", id)
+        })
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let conn = Connection::open("bookmark.db")?;
+
+    let sql = "CREATE TABLE IF NOT EXISTS bookmark (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        url TEXT UNIQUE,
+        created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )";
+    let _ = conn.execute(sql, ());
+
+    let shared_conn = Arc::new(Mutex::new(conn));
+
+
+    let cors_layer = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods([Method::POST])
+        .allow_headers([hyper::header::CONTENT_TYPE]);
+    let middleware = tower::ServiceBuilder::new().layer(cors_layer);
+
+
+    let addr = SocketAddr::from(([127,0,0,1], 6644));
+    let server = Server::builder()
+        .set_http_middleware(middleware)
+        .build(addr)
+        .await?;
+
+    let bookmark_repository = BookmarkRepository{
+        repo: shared_conn
+    };
+    let module = RpcImplent{
+        bookmark_repo: bookmark_repository 
+    }.into_rpc();
+
+    let handle = server.start(module);
+    println!("server starting at {}", addr);
+    handle.stopped().await;
+    Ok(())
+}
+
---
+++ src/repository.rs
@@ -0,0 +1,75 @@
+use std::{error::Error, sync::{Arc, Mutex}};
+
+use rusqlite::{Connection, Result};
+use serde::Serialize;
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Bookmark{
+    pub id: i16,
+    pub title: String,
+    pub url : String
+}
+
+pub struct BookmarkRepository{
+    pub repo: Arc<Mutex<Connection>>,
+}
+
+impl BookmarkRepository{
+    pub fn get(&self) -> Result<Vec<Bookmark>, Box<dyn Error>> {
+        // let mut bookmarks = Vec::new();
+
+        let query = "SELECT * FROM bookmark";
+
+        let db = self.repo.lock().map_err(|_| "failed lock db to process")?;
+
+        let mut prepare = db.prepare(query)?;
+
+        
+        let bookmark_iter = prepare.query_map([], |row|{
+            Ok(Bookmark{
+                id: row.get(0)?,
+                title: row.get(1)?,
+                url: row.get(2)?
+            })
+        }).map_err(|e| e.to_string())?;
+
+        let bookmarks = bookmark_iter.collect::<Result<Vec<Bookmark>, rusqlite::Error>>()?;
+
+        Ok(bookmarks)
+
+    }
+
+    pub fn add(&self, title: String, url: String)-> Result<String, Box<dyn Error>>{
+        let query = "INSERT INTO bookmark (title, url) VALUES(?1,?2)";
+
+        let db = self.repo.lock().map_err(|_| "failed lock db to process")?;
+        match db.execute(query, (title, url)) {
+            Ok(_)=> {
+                Ok("success add url".to_string())
+            }
+
+            Err(e)=>{
+                eprintln!("Error: {}", e);
+                Err("failed add url to bookmark".into())
+            }
+        }
+
+    }
+
+    pub fn delete(&self, id: String) -> Result<String, Box<dyn Error>>{
+        let query = "DELETE FROM bookmark WHERE id=?1";
+
+        let db = self.repo.lock().map_err(|_| "failed lock database")?;
+
+        match db.execute(query, (id,)) {
+            Ok(_)=> {
+                Ok("success delete bookmark".to_string())
+            }
+            Err(e)=>{
+                eprintln!("Error: {}", e);
+                Err("failed delete bookmark".into())
+            }
+        }
+
+    }
+}
