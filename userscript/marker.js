c517f1b7206a13dbf7fc1ee7d98bf9032622555d
Wisnu Box <hwisnu222@gmail.com>
2026-07-06 19:38:52 +0700

    fix: button does't appear in spa

--- userscript/marker.js
+++ userscript/marker.js
@@ -13,81 +13,98 @@
 // ==/UserScript==
 
 (function () {
-  let container = document.createElement("div");
+  const addButton = (obs) => {
+    const target = document.querySelector(".container-marker");
+    if (!target) {
+      console.log("button added!");
+      obs.disconnect();
+      let container = document.createElement("div");
 
-  container.style.display = "flex";
-  container.style.justifyContent = "center";
-  container.style.alignItems = "center";
-  container.style.position = "fixed";
-  container.style.top = "5em";
-  container.style.right = "1em";
+      container.className = "container-marker";
+      container.style.display = "flex";
+      container.style.justifyContent = "center";
+      container.style.alignItems = "center";
+      container.style.position = "fixed";
+      container.style.top = "5em";
+      container.style.right = "1em";
+      container.style.zIndex = 1000;
 
-  let button = document.createElement("button");
-  button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark-icon lucide-bookmark"><path d="M17 3a2 2 0 0 1 2 2v15a1 1 0 0 1-1.496.868l-4.512-2.578a2 2 0 0 0-1.984 0l-4.512 2.578A1 1 0 0 1 5 20V5a2 2 0 0 1 2-2z"/></svg>`;
-  button.style.padding = "0.8em";
-  button.style.background = "#000000";
-  button.style.color = "#ffffff";
-  button.style.borderRadius = "5px";
-  button.style.border = "none";
-  button.style.outline = "none";
+      let button = document.createElement("button");
+      button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark-icon lucide-bookmark"><path d="M17 3a2 2 0 0 1 2 2v15a1 1 0 0 1-1.496.868l-4.512-2.578a2 2 0 0 0-1.984 0l-4.512 2.578A1 1 0 0 1 5 20V5a2 2 0 0 1 2-2z"/></svg>`;
+      button.style.padding = "0.8em";
+      button.style.background = "#000000";
+      button.style.color = "#ffffff";
+      button.style.borderRadius = "5px";
+      button.style.border = "none";
+      button.style.outline = "none";
 
-  container.appendChild(button);
+      container.appendChild(button);
 
-  document.body.append(container);
+      document.body.append(container);
 
-  button.addEventListener("click", function () {
-    const title = document.title;
-    const url = document.location.href;
-    const id = new Date().getTime();
+      button.addEventListener("click", function () {
+        const title = document.title;
+        const url = document.location.href;
+        const id = new Date().getTime();
 
-    const body = {
-      id: id,
-      jsonrpc: "2.0",
-      method: "add_url",
-      params: [title, url],
-    };
+        const body = {
+          id: id,
+          jsonrpc: "2.0",
+          method: "add_url",
+          params: [title, url],
+        };
 
-    console.log(body);
+        console.log(body);
 
-    GM_xmlhttpRequest({
-      method: "POST",
-      url: "http://127.0.0.1:6644",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify(body),
-      onload: (res) => {
-        try {
-          const data = JSON.parse(res.responseText);
-          console.log(data);
+        GM_xmlhttpRequest({
+          method: "POST",
+          url: "http://armbian.local:6644",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          data: JSON.stringify(body),
+          onload: (res) => {
+            try {
+              const data = JSON.parse(res.responseText);
+              console.log(data);
 
-          if (data?.error) {
+              if (data?.error) {
+                GM_notification({
+                  title: "Error",
+                  text: data.error.message,
+                });
+                return;
+              }
+
+              GM_notification({
+                title: "Success",
+                text: data.result.message,
+              });
+            } catch (data) {
+              GM_notification({
+                title: "Error",
+                text: "can't parse response",
+              });
+            }
+          },
+          onerror: (res) => {
             GM_notification({
               title: "Error",
-              text: data.error.message,
+              text: "can't connect to bookmark server",
             });
-            return;
-          }
-
-          GM_notification({
-            title: "Success",
-            text: data.result.message,
-          });
-        } catch (err) {
-          console.log(err);
-          GM_notification({
-            title: "Error",
-            text: "can't parse response",
-          });
-        }
-      },
-      onerror: (err) => {
-        print(err);
-        GM_notification({
-          title: "Error",
-          text: "can't connect to bookmark server",
+          },
         });
-      },
-    });
+      });
+    }
+  };
+
+  const observer = new MutationObserver((mutation, obs) => {
+    console.log("dom is change");
+    addButton(obs);
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
   });
 })();
